782270c97870ee4b29e83eebe6fa4785f4aa385b
Eric Caleb <calebme@mail.com>
2016-09-02 11:19:57 +0100

    Added start command message

--- snowgeek.rb
+++ snowgeek.rb
@@ -1,3 +1,13 @@
 require 'telegram/bot'
-token = '233188694:AAGblcCE_vKtMAK1MhSv7QXhRjeYLpcuVTM
-'
+token = '261677858:AAEF1azDHFrS51BiVeZPqWa0gCKAin-ixMQ'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/caleb'
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome to https://iamcaleberic.github.io")
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "I am the geeksnow bot, My commands are /caleb /map")
+    end
+  end
+end
